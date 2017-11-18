b2df945338df799343bc4d9eb7f2fa7dede5e38c
Marcos Lisboa <marcosplisboa@yahoo.com.br>
2017-11-18 19:05:43 -0200

    Initial Commit

---
+++ src/main/test/br/com/webapp/dao/ProductDAOTest.java
@@ -0,0 +1,34 @@
+package br.com.webapp.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import junit.framework.TestCase;
+
+
+@RunWith(JUnit4.class)
+public class ProductDAOTest {
+
+    @Test
+    public void testFindAll() {
+
+    }
+
+    @Test
+    public void testFindById() {
+    }
+
+    @Test
+    public void testInsert() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+}
