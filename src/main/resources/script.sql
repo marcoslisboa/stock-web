b1a745b34f816e47edab27c912c827b154fd1ca9
Marcos Lisboa <marcosplisboa@yahoo.com.br>
2017-11-18 18:59:11 -0200

    Initial Commit

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,9 +1,7 @@
 CREATE DATABASE estoque;
 
 CREATE TABLE t_produto (
-  id          BIGSERIAL NOT NULL
-    CONSTRAINT t_produto_pkey
-    PRIMARY KEY,
+  id          BIGSERIAL NOT NULL PRIMARY KEY,
   name        VARCHAR(255),
   description VARCHAR(500),
   price       NUMERIC(12, 2)
